a07e2216f67fd6dc8eae2a66d2dabcf0a6c0906c
James Parramore <jrp1123@ufl.edu>
2018-11-11 02:40:35 -0500

    added favController, added event update and event delete

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -27,3 +27,40 @@ exports.list = function(req, res) {
     }
   });
 };
+
+// delete an event
+exports.delete = function(req, res) {
+
+  var event = req.event;
+  event.remove(function(err){
+    if(err){
+      console.log(err);
+      res.status(400).send(err);
+    }
+    else{
+      res.end();
+    }
+  })
+};
+
+exports.update = function(req, res) {
+  var event = req.event;
+
+  event.name = req.body.name;
+  event.address = req.body.address;
+  event.room = req.body.room;
+  event.owner = req.body.owner;
+  event.date = req.body.date;
+  event.time = req.body.time;
+  event.food = req.body.food;
+  event.description = req.body.description;
+
+  event.save(function(err) {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else {
+      res.json(event);
+    }
+  });
+};
