7db6aff92951c5629c4eb43018aa2ad3fe53c10b
Mohammad Fallah <mohammadafall1h@ufl.edu>
2018-11-14 13:10:28 -0500

    config MongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,8 @@ module.exports = {
   db: {
     //uri: 'mongodb://terabite:afewcookie5@ds247852.mlab.com:47852/terabite'
     uri: 'mongodb://test:testpw1@ds143451.mlab.com:43451/terabite'
-  }
+  
+  },
+  port: process.env.PORT || 8080
+
 };
